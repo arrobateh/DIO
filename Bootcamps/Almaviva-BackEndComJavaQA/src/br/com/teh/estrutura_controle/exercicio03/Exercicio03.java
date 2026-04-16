0de734510bb92dd249f355da1e9073311065cd4c
arrobateh <wallaceteh1@gmail.com>
2026-04-15 22:24:58 -0300

    feat: cria a resolucao do exercicio 1

--- Bootcamps/Almaviva-BackEndComJavaQA/src/br/com/teh/estrutura_controle/exercicio03/Exercicio03.java
+++ Bootcamps/Almaviva-BackEndComJavaQA/src/br/com/teh/estrutura_controle/exercicio03/Exercicio03.java
@@ -1,4 +1,40 @@
 package br.com.teh.estrutura_controle.exercicio03;
 
+import java.util.Scanner;
+
 public class Exercicio03 {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+
+        int numUm;
+        int numDois;
+        int opcao;
+
+        System.out.print("Digite um numero: ");
+        numUm = leia.nextInt();
+        System.out.print("Digite outro numero maior que o anterior: ");
+        numDois = leia.nextInt();
+
+        System.out.println("OPCOES");
+        System.out.println("1 - Impar");
+        System.out.println("2 - Par");
+        System.out.print("Escolha uma opção: ");
+        opcao = leia.nextInt();
+
+        switch (opcao) {
+            case 1:
+                for(int i = numDois; i >= numUm; i--){
+                    if (i % 2 != 0){
+                        System.out.print(i + " ");
+                    }
+                }
+                break;
+            case 2:
+                for(int i = numDois; i >= numUm; i--){
+                    if (i % 2 == 0){
+                        System.out.print(i + " ");
+                    }
+                }
+        }
+    }
 }
