c00f55cbffa1d280f6e41eb8f4e72c3f1a817f34
arrobateh <wallaceteh1@gmail.com>
2026-04-15 23:33:22 -0300

    feat: cria a resolucao do exercicio 4

--- Bootcamps/Almaviva-BackEndComJavaQA/src/br/com/teh/estrutura_controle/exercicio04/Exercicio04.java
+++ Bootcamps/Almaviva-BackEndComJavaQA/src/br/com/teh/estrutura_controle/exercicio04/Exercicio04.java
@@ -1,4 +1,20 @@
 package br.com.teh.estrutura_controle.exercicio04;
 
+import java.util.Scanner;
+
 public class Exercicio04 {
+    public static void main(String [] args) {
+        Scanner leia = new Scanner(System.in);
+
+        System.out.print("Digite um numero: ");
+        double numUm = leia.nextDouble();
+
+        double numDois;
+
+        do {
+            System.out.print("Digite outro numero: ");
+            numDois = leia.nextDouble();
+
+        } while (numDois % numUm == 0);
+    }
 }
